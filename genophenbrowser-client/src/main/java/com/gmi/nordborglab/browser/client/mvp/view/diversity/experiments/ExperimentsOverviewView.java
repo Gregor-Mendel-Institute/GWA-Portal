4fbe2a2d3d3b8c63767a24fb7864369413e8d439
Uemit Seren <uemit.seren+git@gmail.com>
2012-11-21 16:54:25 +0100

    Changed from CellTable to DataGrid using new Pager

--- genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/mvp/view/diversity/experiments/ExperimentsOverviewView.java
+++ genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/mvp/view/diversity/experiments/ExperimentsOverviewView.java
@@ -1,23 +1,20 @@
 package com.gmi.nordborglab.browser.client.mvp.view.diversity.experiments;
 
 
+
 import com.gmi.nordborglab.browser.client.NameTokens;
 import com.gmi.nordborglab.browser.client.ParameterizedPlaceRequest;
 import com.gmi.nordborglab.browser.client.mvp.handlers.ExperimentsOverviewUiHandlers;
 import com.gmi.nordborglab.browser.client.mvp.presenter.diversity.experiments.ExperimentsOverviewPresenter;
-import com.gmi.nordborglab.browser.client.mvp.view.diversity.experiments.ExperimentListDataGridColumns.CommentsColumn;
-import com.gmi.nordborglab.browser.client.mvp.view.diversity.experiments.ExperimentListDataGridColumns.DesignColumn;
-import com.gmi.nordborglab.browser.client.mvp.view.diversity.experiments.ExperimentListDataGridColumns.NameColumn;
-import com.gmi.nordborglab.browser.client.mvp.view.diversity.experiments.ExperimentListDataGridColumns.OriginatorColumn;
+import com.gmi.nordborglab.browser.client.ui.CustomPager;
 import com.gmi.nordborglab.browser.shared.proxy.ExperimentProxy;
-import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.DataGrid;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.HasData;
-import com.google.gwt.view.client.ProvidesKey;
 import com.google.inject.Inject;
+import com.google.web.bindery.requestfactory.gwt.ui.client.EntityProxyKeyProvider;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 
@@ -29,25 +26,18 @@ public class ExperimentsOverviewView extends ViewWithUiHandlers<ExperimentsOverv
 	public interface Binder extends UiBinder<Widget, ExperimentsOverviewView> {
 	}
 	
-	public static ProvidesKey<ExperimentProxy> KEY_PROVIDER = new ProvidesKey<ExperimentProxy>() {
-		@Override
-		public Object getKey(ExperimentProxy item) {
-			if (item != null && item.getId() != null) {
-				return item.getId();
-			}
-			return null;
-		}
-	};
 	private final Widget widget;
 	private final PlaceManager placeManager;
 	
-	@UiField(provided=true) CellTable<ExperimentProxy> table;
+	@UiField(provided=true) DataGrid<ExperimentProxy> table;
+	@UiField CustomPager pager;
 
 	@Inject
 	public ExperimentsOverviewView(final Binder binder,final PlaceManager placeManager) {
 		this.placeManager = placeManager;
 		initCellTable();
 		widget = binder.createAndBindUi(this);
+		pager.setDisplay(table);
 	}
 
 	@Override
@@ -58,14 +48,7 @@ public class ExperimentsOverviewView extends ViewWithUiHandlers<ExperimentsOverv
 
 	
 	private void initCellTable() {
-		FieldUpdater<ExperimentProxy, String> updater = new FieldUpdater<ExperimentProxy, String>() {
-
-			@Override
-			public void update(int index, ExperimentProxy object, String value) {
-				getUiHandlers().loadExperiment(object);
-			}
-		};
-		table = new CellTable<ExperimentProxy>(15,KEY_PROVIDER);
+		table = new DataGrid<ExperimentProxy>(15,new EntityProxyKeyProvider<ExperimentProxy>());
 		table.addColumn(new ExperimentListDataGridColumns.NameColumn(placeManager,new ParameterizedPlaceRequest(NameTokens.experiment)),"Name");
 		table.addColumn(new ExperimentListDataGridColumns.DesignColumn(),"Design");
 		table.addColumn(new ExperimentListDataGridColumns.OriginatorColumn(),"Originator");
