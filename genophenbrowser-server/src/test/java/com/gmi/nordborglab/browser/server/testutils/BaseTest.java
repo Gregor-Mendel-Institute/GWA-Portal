ef5315ae2148db11133e01646408819ecf82efd8
Uemit Seren <uemit.seren+git@gmail.com>
2012-11-21 16:51:25 +0100

    Set ActiveProfile to dev in BaseTest

--- genophenbrowser-server/src/test/java/com/gmi/nordborglab/browser/server/testutils/BaseTest.java
+++ genophenbrowser-server/src/test/java/com/gmi/nordborglab/browser/server/testutils/BaseTest.java
@@ -1,12 +1,14 @@
 package com.gmi.nordborglab.browser.server.testutils;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles(profiles = "dev")
 @ContextConfiguration(locations={
 		"classpath:META-INF/applicationContext.xml",
 		"classpath:META-INF/spring-security.xml",
