7b69e238e650f20a60ceddb795255fdef341f250
Uemit Seren <uemit.seren+git@gmail.com>
2013-03-08 10:54:22 +0100

    added ImageResource for thalina and lyrata

--- genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/resources/MainResources.java
+++ genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/resources/MainResources.java
@@ -5,78 +5,78 @@ import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ImageResource;
 
 public interface MainResources extends ClientBundle{
-	
-	
-	public interface MainStyle extends CssResource {
-		String transplant_logo_footer();
-		String eu_logo_footer();
-		String fp7_logo_footer();
-		String gmi_logo_footer();
-		String arrow_down();
-		String avatar();
-		String logout();
-		String profile();
-		String button();
-		String button_grey();
-		String button_blue();
-		String button_red();
-		String cursor();
-		String piechart();
-		String earth();
-		String columnchart();
-		String motionchart();
-		String chartActionRow();
-		String chartIconContainer();
-		String iconContainer();
-		String iconItem();
-		String iconContainer_right();
-		String iconContainer_active();
-		String plant_icon();
-	}
 
+
+    public interface MainStyle extends CssResource {
+
+        String transplant_logo_footer();
+        String eu_logo_footer();
+        String fp7_logo_footer();
+        String gmi_logo_footer();
+        String arrow_down();
+        String avatar();
+        String logout();
+        String profile();
+        String button();
+        String button_grey();
+        String button_blue();
+        String button_red();
+        String cursor();
+        String piechart();
+        String earth();
+        String columnchart();
+        String motionchart();
+        String chartActionRow();
+        String chartIconContainer();
+        String iconContainer();
+        String iconItem();
+        String iconContainer_right();
+        String iconContainer_active();
+        String plant_icon();
+    }
 	@Source("images/transplant_logo_small.png")
 	ImageResource transplant_logo_footer();
-	
+
 	@Source("images/fp7_logo_small.png")
 	ImageResource fp7_logo_footer();
-	
-	
+
 	@Source("images/eu_logo_small.png")
 	ImageResource eu_logo_footer();
-	
+
+
 	@Source("images/gmi_logo_small.png")
 	ImageResource gmi_logo_footer();
-	
+
 	@Source("images/arrow_down.png")
 	ImageResource arrow_down();
-	
+
 	@Source("images/avatar.png")
 	ImageResource avatar();
-	
+
 	@Source("images/logout.png")
 	ImageResource logout();
-	
+
 	@Source("images/profile.png")
 	ImageResource profile();
-	
+
 	@Source("images/piechart.jpg")
 	ImageResource piechart();
-	
+
 	@Source("images/earth.png")
 	ImageResource earth();
-	
+
 	@Source("images/motionchart.png")
 	ImageResource motionchart();
-	
+
 	@Source("images/columnchart.png")
 	ImageResource columnchart();
-	
+
 	@Source("images/plant1.png")
 	ImageResource plant1();
-	
+
 	@Source("images/plant2.png")
 	ImageResource plant2();
-	
+
 	@Source("images/plant3.png")
 	ImageResource plant3();
 
@@ -85,7 +85,13 @@ public interface MainResources extends ClientBundle{
 
     @Source("images/file_add.png")
     ImageResource file_add();
-	
+
+    @Source("images/thaliana.png")
+    ImageResource getThalianaImage();
+
+    @Source("images/thaliana.png")
+    ImageResource getLyrataImage();
+
 	@Source("style.css")
 	MainStyle style();
 }
