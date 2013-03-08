18dc3f1ceea110d0b100cbf81a4085ddb9db5d9d
Uemit Seren <uemit.seren+git@gmail.com>
2013-03-08 10:55:45 +0100

    added LoadTaxonomiesEvent

---
+++ genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/events/LoadTaxonomiesEvent.java
@@ -0,0 +1,56 @@
+package com.gmi.nordborglab.browser.client.events;
+
+import com.gmi.nordborglab.browser.shared.proxy.ExperimentProxy;
+import com.gmi.nordborglab.browser.shared.proxy.TaxonomyProxy;
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HasHandlers;
+import com.google.web.bindery.event.shared.Event;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: uemit.seren
+ * Date: 3/4/13
+ * Time: 1:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoadTaxonomiesEvent extends GwtEvent<LoadTaxonomiesEvent.Handler> {
+
+
+    public interface Handler extends EventHandler {
+        public void onLoadTaxonomies(LoadTaxonomiesEvent event);
+    }
+
+    public static final Type<LoadTaxonomiesEvent.Handler> TYPE = new Type<LoadTaxonomiesEvent.Handler>();
+
+    private final List<TaxonomyProxy> taxonomies;
+
+    public LoadTaxonomiesEvent(List<TaxonomyProxy> taxonomies) {
+        this.taxonomies = taxonomies;
+    }
+
+    @Override
+    public Type<LoadTaxonomiesEvent.Handler> getAssociatedType() {
+        return TYPE;
+    }
+
+    public static Type<LoadTaxonomiesEvent.Handler> getType() {
+        return TYPE;
+    }
+
+    @Override
+    protected void dispatch(LoadTaxonomiesEvent.Handler handler) {
+        handler.onLoadTaxonomies(this);
+    }
+
+    public static void fire(final HasHandlers source,
+                            List<TaxonomyProxy> taxonomies) {
+        source.fireEvent(new LoadTaxonomiesEvent(taxonomies));
+    }
+
+    public List<TaxonomyProxy> getTaxonomies() {
+        return taxonomies;
+    }
+}
