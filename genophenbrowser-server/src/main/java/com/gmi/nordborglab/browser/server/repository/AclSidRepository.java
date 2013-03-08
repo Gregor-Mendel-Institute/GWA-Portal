2d2cd1d7ab28863c3cbac58f21c8462c825e5201
Uemit Seren <uemit.seren+git@gmail.com>
2013-03-08 10:44:26 +0100

    find by id added and bugfix for type

--- genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/repository/AclSidRepository.java
+++ genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/repository/AclSidRepository.java
@@ -1,7 +1,9 @@
 package com.gmi.nordborglab.browser.server.repository;
 
+import com.gmi.nordborglab.browser.server.domain.acl.AclSid;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AclSidRepository extends JpaRepository<AclSidRepository, Long>{
+public interface AclSidRepository extends JpaRepository<AclSid, Long>{
 
+    AclSid findBySid(String sid);
 }
