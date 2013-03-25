8b00a8c83b922e2017660ddf6710f8eae562f873
Uemit Seren <uemit.seren+git@gmail.com>
2013-03-25 14:04:09 +0100

    add flag to specify if link should open in new window

--- genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/ui/cells/HyperlinkCell.java
+++ genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/ui/cells/HyperlinkCell.java
@@ -11,26 +11,35 @@ import com.google.gwt.safehtml.shared.UriUtils;
 public class HyperlinkCell extends AbstractCell<String[]> {
 
 	interface Template extends SafeHtmlTemplates {
-		@Template("<a href=\"{0}\">{1}</a>")
-		SafeHtml hyperText(SafeUri link, String text);
+		@Template("<a href=\"{0}\" target=\"{2}\">{1}</a>")
+		SafeHtml hyperText(SafeUri link, String text,String target);
 	}
 
 	private static Template template = GWT.create(Template.class);;
+    private final boolean isNewWindow;
 
 	public static final int LINK_INDEX = 0, URL_INDEX = 1;
 
 	/**
 	 * Construct a new ImageCell.
 	 */
+
+    public HyperlinkCell(boolean isNewWindow) {
+       this.isNewWindow = isNewWindow;
+    }
 	public HyperlinkCell() {
+        this(false);
 	}
 
 	@Override
 	public void render(Context context, String[] value, SafeHtmlBuilder sb) {
 		if (value != null) {
+            String target = "";
+            if (isNewWindow)
+                target = "_blank";
 			// The template will sanitize the URI.
 			sb.append(template.hyperText(
-					UriUtils.fromString(value[LINK_INDEX]), value[URL_INDEX]));
+					UriUtils.fromString(value[LINK_INDEX]), value[URL_INDEX],target));
 		}
 	}
 }
