422f74874cf0a5033f0065dad85fb2c77efe7876
Ümit Seren <uemit.seren@gmail.com>
2013-06-28 11:59:52 +0200

    Refactor (Move) Stats DTO's into stats package

--- genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/service/TaxonomyService.java
+++ genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/service/TaxonomyService.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 
-import com.gmi.nordborglab.browser.server.domain.TaxonomyStats;
+import com.gmi.nordborglab.browser.server.domain.stats.TaxonomyStats;
 import com.gmi.nordborglab.browser.server.domain.germplasm.Taxonomy;
 
 public interface TaxonomyService {
-	
-	public List<Taxonomy> findAll();
-	
-	public Taxonomy findOne(Long id);
-	
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public Taxonomy save(Taxonomy taxonomy);
-	
-	public TaxonomyStats findStats(Long id);
+
+    public List<Taxonomy> findAll();
+
+    public Taxonomy findOne(Long id);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Taxonomy save(Taxonomy taxonomy);
+
+    public TaxonomyStats findStats(Long id);
 
 }
--- genophenbrowser-server/src/test/java/com/gmi/nordborglab/browser/server/service/PassportServiceTest.java
+++ genophenbrowser-server/src/test/java/com/gmi/nordborglab/browser/server/service/PassportServiceTest.java
@@ -13,7 +13,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.gmi.nordborglab.browser.server.domain.PassportSearchCriteria;
-import com.gmi.nordborglab.browser.server.domain.PassportStats;
+import com.gmi.nordborglab.browser.server.domain.stats.PassportStats;
 import com.gmi.nordborglab.browser.server.domain.germplasm.Passport;
 import com.gmi.nordborglab.browser.server.domain.germplasm.Stock;
 import com.gmi.nordborglab.browser.server.domain.pages.PassportPage;
@@ -22,82 +22,82 @@ import com.gmi.nordborglab.browser.server.testutils.SecurityUtils;
 
 public class PassportServiceTest extends BaseTest {
 
-	@Resource
-	private PassportService service;
-	
-	@Before
-	public void setUp() {
-	}
+    @Resource
+    private PassportService service;
 
-	@After
-	public void clearContext() {
-		SecurityUtils.clearContext();
-	}
+    @Before
+    public void setUp() {
+    }
 
-	@Test
-	public void testFindAll() {
-		PassportSearchCriteria filter = new PassportSearchCriteria();
-		PassportPage page = service.findAll(1L,filter, 0, 50);
-		assertEquals(7064, page.getTotalElements());
-		assertEquals(50, page.getNumberOfElements());
-		assertEquals(50, page.getContent().size());
-	}
-	
-	@Test
-	public void testFindAllByPassportId() {
-		PassportSearchCriteria filter = new PassportSearchCriteria();
-		filter.setPassportId(1L);
-		PassportPage page = service.findAll(1L,filter, 0, 50);
-		assertEquals(1, page.getTotalElements());
-		assertEquals(1, page.getNumberOfElements());
-		assertEquals(1, page.getContent().size());
-	}
-	
-	@Test
-	public void testFindAllByCountry() {
-		PassportSearchCriteria filter = new PassportSearchCriteria();
-		List<String> countries = new ArrayList<String>();
-		countries.add("FRA");
-		filter.setCountries(countries);
-		PassportPage page = service.findAll(1L,filter, 0, 50);
-		assertEquals(444, page.getTotalElements());
-		assertEquals(50, page.getNumberOfElements());
-		assertEquals(50, page.getContent().size());
-	}
-	
-	@Test
-	public void testFindAllByCountries() {
-		PassportSearchCriteria filter = new PassportSearchCriteria();
-		List<String> countries = new ArrayList<String>();
-		countries.add("FRA");
-		countries.add("GER");
-		filter.setCountries(countries);
-		PassportPage page = service.findAll(1L,filter, 0, 50);
-		assertEquals(712, page.getTotalElements());
-		assertEquals(50, page.getNumberOfElements());
-		assertEquals(50, page.getContent().size());
-	}
-	
-	@Test
-	public void testFindOne() {
-		Passport passport = service.findOne(1L);
-		assertNotNull("no passport found",passport);
-		assertEquals("id wrong",1,passport.getId().longValue());
-	}
-	
-	@Test
-	public void testFindAllStocks() {
-		List<Stock> stocks = service.findAllStocks(1L);
-		assertNotNull("No stocks found",stocks);
-		assertEquals("wrong number of stocks",1,stocks.size());
-		Stock stock = stocks.get(0);
-		assertEquals("wrong associated passport",1,stock.getPassport().getId().longValue());
-	}
-	
-	@Test
-	public void testFindStats() {
-		PassportStats stats = service.findStats(1L);
-		assertNotNull(stats);
-		assertNotNull(stats.getData());
-	}
+    @After
+    public void clearContext() {
+        SecurityUtils.clearContext();
+    }
+
+    @Test
+    public void testFindAll() {
+        PassportSearchCriteria filter = new PassportSearchCriteria();
+        PassportPage page = service.findAll(1L, filter, 0, 50);
+        assertEquals(7064, page.getTotalElements());
+        assertEquals(50, page.getNumberOfElements());
+        assertEquals(50, page.getContent().size());
+    }
+
+    @Test
+    public void testFindAllByPassportId() {
+        PassportSearchCriteria filter = new PassportSearchCriteria();
+        filter.setPassportId(1L);
+        PassportPage page = service.findAll(1L, filter, 0, 50);
+        assertEquals(1, page.getTotalElements());
+        assertEquals(1, page.getNumberOfElements());
+        assertEquals(1, page.getContent().size());
+    }
+
+    @Test
+    public void testFindAllByCountry() {
+        PassportSearchCriteria filter = new PassportSearchCriteria();
+        List<String> countries = new ArrayList<String>();
+        countries.add("FRA");
+        filter.setCountries(countries);
+        PassportPage page = service.findAll(1L, filter, 0, 50);
+        assertEquals(444, page.getTotalElements());
+        assertEquals(50, page.getNumberOfElements());
+        assertEquals(50, page.getContent().size());
+    }
+
+    @Test
+    public void testFindAllByCountries() {
+        PassportSearchCriteria filter = new PassportSearchCriteria();
+        List<String> countries = new ArrayList<String>();
+        countries.add("FRA");
+        countries.add("GER");
+        filter.setCountries(countries);
+        PassportPage page = service.findAll(1L, filter, 0, 50);
+        assertEquals(712, page.getTotalElements());
+        assertEquals(50, page.getNumberOfElements());
+        assertEquals(50, page.getContent().size());
+    }
+
+    @Test
+    public void testFindOne() {
+        Passport passport = service.findOne(1L);
+        assertNotNull("no passport found", passport);
+        assertEquals("id wrong", 1, passport.getId().longValue());
+    }
+
+    @Test
+    public void testFindAllStocks() {
+        List<Stock> stocks = service.findAllStocks(1L);
+        assertNotNull("No stocks found", stocks);
+        assertEquals("wrong number of stocks", 1, stocks.size());
+        Stock stock = stocks.get(0);
+        assertEquals("wrong associated passport", 1, stock.getPassport().getId().longValue());
+    }
+
+    @Test
+    public void testFindStats() {
+        PassportStats stats = service.findStats(1L);
+        assertNotNull(stats);
+        assertNotNull(stats.getData());
+    }
 }
