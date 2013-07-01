b1cd245378d223374dd2e43a856e4bc852cca93a
Ümit Seren <uemit.seren@gmail.com>
2013-07-01 10:40:41 +0200

    genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/domain/AppStat.java

---
+++ genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/domain/AppStat.java
@@ -0,0 +1,11 @@
+package com.gmi.nordborglab.browser.server.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: uemit.seren
+ * Date: 25.06.13
+ * Time: 10:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppStat {
+}
