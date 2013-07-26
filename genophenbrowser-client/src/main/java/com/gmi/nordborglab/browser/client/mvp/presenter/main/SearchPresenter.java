7f76687a8328bdfa2aac95ed3210cc4fc5643f83
Ümit Seren <uemit.seren@gmail.com>
2013-07-26 10:58:18 +0200

    rename experiment to study and study to analysis

--- genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/mvp/presenter/main/SearchPresenter.java
+++ genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/mvp/presenter/main/SearchPresenter.java
@@ -28,106 +28,106 @@ import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.PlaceRequest;
 
 
-public class SearchPresenter extends PresenterWidget<SearchPresenter.MyView> implements SearchUiHandlers{
+public class SearchPresenter extends PresenterWidget<SearchPresenter.MyView> implements SearchUiHandlers {
 
-	public interface MyView extends View,HasUiHandlers<SearchUiHandlers> {
+    public interface MyView extends View, HasUiHandlers<SearchUiHandlers> {
 
-		void setMinCharSize(int minCharSize);
-	}
-	
-	private CATEGORY category;
-	private final CustomRequestFactory rf;
-	private final PlaceManager placeManager;
+        void setMinCharSize(int minCharSize);
+    }
 
-	@Inject
-	public SearchPresenter(final EventBus eventBus, final MyView view, 
-			final CustomRequestFactory rf, final PlaceManager placeManager) {
-		super(eventBus, view);
-		this.rf = rf;
-		this.placeManager = placeManager;
-		getView().setUiHandlers(this);
-	}
+    private CATEGORY category;
+    private final CustomRequestFactory rf;
+    private final PlaceManager placeManager;
 
-	@Override
-	protected void onBind() {
-		super.onBind();
-	}
+    @Inject
+    public SearchPresenter(final EventBus eventBus, final MyView view,
+                           final CustomRequestFactory rf, final PlaceManager placeManager) {
+        super(eventBus, view);
+        this.rf = rf;
+        this.placeManager = placeManager;
+        getView().setUiHandlers(this);
+    }
 
-	@Override
-	protected void onReset() {
-		super.onReset();
-	}
-	
-	public void setCategory(CATEGORY category) {
-		this.category = category;
-	}
-	
-	public void setMinCharSize(int minCharSize) {
-		getView().setMinCharSize(minCharSize);
-	}
+    @Override
+    protected void onBind() {
+        super.onBind();
+    }
 
-	@Override
-	public void onRequestSearch(final Request request, final Callback callback) {
-		rf.searchRequest().searchByTerm(request.getQuery(), category, null).fire(new Receiver<List<SearchFacetPageProxy>>() {
+    @Override
+    protected void onReset() {
+        super.onReset();
+    }
 
-			@Override
-			public void onSuccess(List<SearchFacetPageProxy> response) {
-				Response searchResponse = new Response();
-				Collection<Suggestion> suggestions = new ArrayList<Suggestion>();
-				for (SearchFacetPageProxy facetPage: response) {
-					for (SearchItemProxy searchItem:facetPage.getContents()) {
-						suggestions.add(new SearchSuggestOracle.SearchSuggestion(searchItem));
-					}
-				}
-				searchResponse.setSuggestions(suggestions);
-				callback.onSuggestionsReady(request, searchResponse);
-			}
-		});
-	}
+    public void setCategory(CATEGORY category) {
+        this.category = category;
+    }
 
-	@Override
-	public void onNavigateToSuggestion(SearchSuggestion searchSuggestion) {
-		String nameToken = null;
-		switch (searchSuggestion.getCategory()) {
-			case EXPERIMENT:
-				nameToken = NameTokens.experiment;
-				break;
-			case PHENOTYPE:
-				nameToken = NameTokens.phenotype;
-				break;
-			case STOCK:
-				nameToken = NameTokens.stock;
-				break;
-			case TAXONOMY:
-				nameToken = NameTokens.taxonomy;
-				break;
-			case PASSPORT:
-				nameToken = NameTokens.passport;
-				break;
-			case STUDY:
-				nameToken = NameTokens.study;
-				break;
-			case ONTOLOGY:
-				nameToken = NameTokens.traitontology;
-				break;
+    public void setMinCharSize(int minCharSize) {
+        getView().setMinCharSize(minCharSize);
+    }
+
+    @Override
+    public void onRequestSearch(final Request request, final Callback callback) {
+        rf.searchRequest().searchByTerm(request.getQuery(), category, null).fire(new Receiver<List<SearchFacetPageProxy>>() {
+
+            @Override
+            public void onSuccess(List<SearchFacetPageProxy> response) {
+                Response searchResponse = new Response();
+                Collection<Suggestion> suggestions = new ArrayList<Suggestion>();
+                for (SearchFacetPageProxy facetPage : response) {
+                    for (SearchItemProxy searchItem : facetPage.getContents()) {
+                        suggestions.add(new SearchSuggestOracle.SearchSuggestion(searchItem));
+                    }
+                }
+                searchResponse.setSuggestions(suggestions);
+                callback.onSuggestionsReady(request, searchResponse);
+            }
+        });
+    }
+
+    @Override
+    public void onNavigateToSuggestion(SearchSuggestion searchSuggestion) {
+        String nameToken = null;
+        switch (searchSuggestion.getCategory()) {
+            case STUDY:
+                nameToken = NameTokens.experiment;
+                break;
+            case PHENOTYPE:
+                nameToken = NameTokens.phenotype;
+                break;
+            case STOCK:
+                nameToken = NameTokens.stock;
+                break;
+            case TAXONOMY:
+                nameToken = NameTokens.taxonomy;
+                break;
+            case PASSPORT:
+                nameToken = NameTokens.passport;
+                break;
+            case ANALYSIS:
+                nameToken = NameTokens.study;
+                break;
+            case ONTOLOGY:
+                nameToken = NameTokens.traitontology;
+                break;
             case PUBLICATION:
                 nameToken = NameTokens.publication;
-		}
-		if (nameToken == null)
-			return;
-		
-		PlaceRequest request = new ParameterizedPlaceRequest(nameToken).with("id", searchSuggestion.getId());
+        }
+        if (nameToken == null)
+            return;
+
+        PlaceRequest request = new ParameterizedPlaceRequest(nameToken).with("id", searchSuggestion.getId());
 
         //TODO refactor
         switch (searchSuggestion.getCategory()) {
             case ONTOLOGY:
                 String ontologyType = "trait";
-                if (searchSuggestion.getId().substring(0,2).equalsIgnoreCase("EO")) {
-                    ontologyType="environment";
+                if (searchSuggestion.getId().substring(0, 2).equalsIgnoreCase("EO")) {
+                    ontologyType = "environment";
                 }
-                request.with("ontology",ontologyType);
+                request.with("ontology", ontologyType);
                 break;
         }
-		placeManager.revealPlace(request);
-	}
+        placeManager.revealPlace(request);
+    }
 }
