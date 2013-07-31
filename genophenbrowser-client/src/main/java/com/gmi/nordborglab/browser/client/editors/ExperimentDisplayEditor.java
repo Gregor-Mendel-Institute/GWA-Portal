352b3f614aaae19d519f2a13e82cd07f68c1fdb4
Ümit Seren <uemit.seren@gmail.com>
2013-07-31 11:06:09 +0200

    Added AccessLabel to display access status

--- genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/editors/ExperimentDisplayEditor.java
+++ genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/editors/ExperimentDisplayEditor.java
@@ -1,5 +1,6 @@
 package com.gmi.nordborglab.browser.client.editors;
 
+import com.gmi.nordborglab.browser.client.ui.AccessLabel;
 import com.gmi.nordborglab.browser.shared.proxy.ExperimentProxy;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.editor.client.Editor;
@@ -11,20 +12,28 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class ExperimentDisplayEditor extends Composite implements Editor<ExperimentProxy> {
 
-	private static ExperimentDisplayEditorUiBinder uiBinder = GWT
-			.create(ExperimentDisplayEditorUiBinder.class);
+    private static ExperimentDisplayEditorUiBinder uiBinder = GWT
+            .create(ExperimentDisplayEditorUiBinder.class);
 
-	interface ExperimentDisplayEditorUiBinder extends
-			UiBinder<Widget, ExperimentDisplayEditor> {
-	}
-	@UiField Label name;
-	@UiField Label design;
-	@UiField Label originator;
-	@UiField Label comments;
-	
+    interface ExperimentDisplayEditorUiBinder extends
+            UiBinder<Widget, ExperimentDisplayEditor> {
+    }
 
-	public ExperimentDisplayEditor() {
-		initWidget(uiBinder.createAndBindUi(this));
-	}
+    @UiField
+    Label name;
+    @UiField
+    Label design;
+    @UiField
+    Label originator;
+    @UiField
+    Label comments;
+    @UiField
+    @Path("public")
+    AccessLabel access;
+
+
+    public ExperimentDisplayEditor() {
+        initWidget(uiBinder.createAndBindUi(this));
+    }
 
 }
