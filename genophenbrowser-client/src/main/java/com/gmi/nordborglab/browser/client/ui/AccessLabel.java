e09b94245a4ac00cf00766ec371cdb912b411300
Ümit Seren <uemit.seren@gmail.com>
2013-07-31 11:05:23 +0200

    Added AccessLabel for displaying Public/Restricted permission

---
+++ genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/ui/AccessLabel.java
@@ -0,0 +1,37 @@
+package com.gmi.nordborglab.browser.client.ui;
+
+import com.github.gwtbootstrap.client.ui.Label;
+import com.github.gwtbootstrap.client.ui.constants.LabelType;
+import com.google.gwt.editor.client.Editor;
+import com.google.gwt.editor.client.IsEditor;
+import com.google.gwt.editor.client.LeafValueEditor;
+import com.google.gwt.user.client.ui.Composite;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: uemit.seren
+ * Date: 30.07.13
+ * Time: 13:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccessLabel extends Composite implements LeafValueEditor<Boolean> {
+    private final Label label = new Label();
+    private boolean value;
+
+    public AccessLabel() {
+        initWidget(label);
+    }
+
+    @Override
+    public void setValue(Boolean value) {
+        label.setType((value ? LabelType.SUCCESS : LabelType.WARNING));
+        label.setText((value ? "PUBLIC" : "RESTRICTED"));
+        this.value = value;
+    }
+
+    @Override
+    public Boolean getValue() {
+        return value;
+    }
+
+}
