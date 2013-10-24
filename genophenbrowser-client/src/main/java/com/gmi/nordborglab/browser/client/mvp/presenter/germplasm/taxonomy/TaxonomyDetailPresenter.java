1e8b2f770172838ff5f0b940bf25a80b7ceb9d1f
Ümit Seren <uemit.seren@gmail.com>
2013-10-24 15:49:40 +0200

    Enhancement: TaxonomyDetailView optimized for all screen resolutions
    * Panel for text-information reduced
    * Explicit height for top panel set -> Term-accession is also shown for low resolutions
    * Minimum height set to 750 and overflow set to auto -> shows a scrollbar if the vertical
    resolution is to small
    * Lower panel split into 30% and 69% -> PieChart looks same as in the top panel.

--- genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/mvp/presenter/germplasm/taxonomy/TaxonomyDetailPresenter.java
+++ genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/mvp/presenter/germplasm/taxonomy/TaxonomyDetailPresenter.java
@@ -1,8 +1,12 @@
 package com.gmi.nordborglab.browser.client.mvp.presenter.germplasm.taxonomy;
 
 import java.util.Set;
+
+import com.googlecode.gwt.charts.client.DataTable;
 import com.gwtplatform.mvp.client.HasUiHandlers;
+
 import javax.validation.ConstraintViolation;
+
 import com.gwtplatform.mvp.client.View;
 import com.gmi.nordborglab.browser.client.CurrentUser;
 import com.gmi.nordborglab.browser.client.NameTokens;
@@ -20,7 +24,6 @@ import com.gmi.nordborglab.browser.shared.proxy.AccessControlEntryProxy;
 import com.gmi.nordborglab.browser.shared.proxy.TaxonomyProxy;
 import com.gmi.nordborglab.browser.shared.proxy.TaxonomyStatsProxy;
 import com.gmi.nordborglab.browser.shared.service.TaxonomyRequest;
-import com.google.gwt.visualization.client.DataTable;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.google.web.bindery.requestfactory.shared.Receiver;
@@ -37,184 +40,183 @@ import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 import com.gwtplatform.mvp.client.proxy.RevealContentEvent;
 
 public class TaxonomyDetailPresenter
-		extends
-		Presenter<TaxonomyDetailPresenter.MyView, TaxonomyDetailPresenter.MyProxy> implements TaxonomyDetailUiHandlers{
+        extends
+        Presenter<TaxonomyDetailPresenter.MyView, TaxonomyDetailPresenter.MyProxy> implements TaxonomyDetailUiHandlers {
 
-	public interface MyView extends View,HasUiHandlers<TaxonomyDetailUiHandlers> {
-		TaxonomyDisplayDriver getDisplayDriver();
+    public interface MyView extends View, HasUiHandlers<TaxonomyDetailUiHandlers> {
+        TaxonomyDisplayDriver getDisplayDriver();
 
-		void setState(State editing, int permission);
+        void setState(State editing, int permission);
 
-		TaxonomyEditDriver getEditDriver();
+        TaxonomyEditDriver getEditDriver();
 
-		void setGeoChartData(DataTable createDataTableFromString);
+        void setGeoChartData(DataTable createDataTableFromString);
 
-		void setAlleleAssayData(DataTable createDataTableFromString);
+        void setAlleleAssayData(DataTable createDataTableFromString);
 
-		void setSampStatData(DataTable createDataTableFromString);
+        void setSampStatData(DataTable createDataTableFromString);
 
-		void scheduleLayout();
+        void scheduleLayout();
 
-		void setStockGenerationData(DataTable createDataTableFromString);
-	}
+        void setStockGenerationData(DataTable createDataTableFromString);
+    }
 
-	@ProxyCodeSplit
-	@NameToken(NameTokens.taxonomy)
-	public interface MyProxy extends ProxyPlace<TaxonomyDetailPresenter> {
-	}
-	
-	private TaxonomyProxy taxonomy;
-	private final PlaceManager placeManager;
-	private boolean fireLoadEvent = false;
-	private final TaxonomyManager taxonomyManager;
-	private final CurrentUser currentUser;
-	protected final Receiver<TaxonomyProxy> receiver;
-	protected TaxonomyStatsProxy stats = null;
+    @ProxyCodeSplit
+    @NameToken(NameTokens.taxonomy)
+    public interface MyProxy extends ProxyPlace<TaxonomyDetailPresenter> {
+    }
 
-	@Inject
-	public TaxonomyDetailPresenter(final EventBus eventBus, final MyView view,
-			final MyProxy proxy, final PlaceManager placeManager, 
-			final TaxonomyManager taxonomyManager,final CurrentUser currentUser) {
-		super(eventBus, view, proxy);
-		getView().setUiHandlers(this);
-		this.placeManager = placeManager;
-		this.taxonomyManager = taxonomyManager;
-		this.currentUser = currentUser;
-		receiver = new Receiver<TaxonomyProxy>() {
-			public void onSuccess(TaxonomyProxy response) {
-				taxonomy = response;
-				fireEvent(new LoadTaxonomyEvent(taxonomy));
-				getView().setState(State.DISPLAYING,getPermission());
-				getView().getDisplayDriver().display(taxonomy);
-			}
+    private TaxonomyProxy taxonomy;
+    private final PlaceManager placeManager;
+    private boolean fireLoadEvent = false;
+    private final TaxonomyManager taxonomyManager;
+    private final CurrentUser currentUser;
+    protected final Receiver<TaxonomyProxy> receiver;
+    protected TaxonomyStatsProxy stats = null;
+
+    @Inject
+    public TaxonomyDetailPresenter(final EventBus eventBus, final MyView view,
+                                   final MyProxy proxy, final PlaceManager placeManager,
+                                   final TaxonomyManager taxonomyManager, final CurrentUser currentUser) {
+        super(eventBus, view, proxy);
+        getView().setUiHandlers(this);
+        this.placeManager = placeManager;
+        this.taxonomyManager = taxonomyManager;
+        this.currentUser = currentUser;
+        receiver = new Receiver<TaxonomyProxy>() {
+            public void onSuccess(TaxonomyProxy response) {
+                taxonomy = response;
+                fireEvent(new LoadTaxonomyEvent(taxonomy));
+                getView().setState(State.DISPLAYING, getPermission());
+                getView().getDisplayDriver().display(taxonomy);
+            }
 
 
-			public void onFailure(ServerFailure error) {
-				fireEvent(new DisplayNotificationEvent("Error while saving",error.getMessage(),true,DisplayNotificationEvent.LEVEL_ERROR,0));
-				onEdit();
-			}
+            public void onFailure(ServerFailure error) {
+                fireEvent(new DisplayNotificationEvent("Error while saving", error.getMessage(), true, DisplayNotificationEvent.LEVEL_ERROR, 0));
+                onEdit();
+            }
 
-			public void onConstraintViolation(
-					Set<ConstraintViolation<?>> violations) {
-				getView().getEditDriver().setConstraintViolations(violations);
-				getView().setState(State.EDITING,getPermission());
-			}
-		};
-		
-	}
+            public void onConstraintViolation(
+                    Set<ConstraintViolation<?>> violations) {
+                getView().getEditDriver().setConstraintViolations(violations);
+                getView().setState(State.EDITING, getPermission());
+            }
+        };
 
-	@Override
-	protected void revealInParent() {
-		RevealContentEvent.fire(this, GermplasmPresenter.TYPE_SetMainContent, this);
-	}
+    }
 
-	@Override
-	protected void onBind() {
-		super.onBind();
-	}
+    @Override
+    protected void revealInParent() {
+        RevealContentEvent.fire(this, GermplasmPresenter.TYPE_SetMainContent, this);
+    }
 
-	@Override
-	protected void onReset() {
-		super.onReset();
-		if (fireLoadEvent) {
-			fireLoadEvent = false;
-			fireEvent(new LoadTaxonomyEvent(taxonomy));
-		}
-		fireEvent(new LoadingIndicatorEvent(false));
-		getView().getDisplayDriver().display(taxonomy);
-		getView().setState(State.DISPLAYING,getPermission());
-		if (stats == null) {
-			taxonomyManager.findStats(new Receiver<TaxonomyStatsProxy>() {
+    @Override
+    protected void onBind() {
+        super.onBind();
+    }
 
-				@Override
-				public void onSuccess(TaxonomyStatsProxy response) {
-					stats = response;
-					displayStats();
-				}
-				
-			}, taxonomy.getId());
-		}
-		else {
-			displayStats();
-		}
-	}
-	
-	@Override
-	public void prepareFromRequest(PlaceRequest placeRequest) {
-		super.prepareFromRequest(placeRequest);
-		LoadingIndicatorEvent.fire(this, true);
-		Receiver<TaxonomyProxy> receiver = new Receiver<TaxonomyProxy>() {
-			@Override
-			public void onSuccess(TaxonomyProxy tax) {
-				taxonomy = tax;
-				fireLoadEvent = true;
-				getProxy().manualReveal(TaxonomyDetailPresenter.this);
-			}
+    @Override
+    protected void onReset() {
+        super.onReset();
+        if (fireLoadEvent) {
+            fireLoadEvent = false;
+            fireEvent(new LoadTaxonomyEvent(taxonomy));
+        }
+        fireEvent(new LoadingIndicatorEvent(false));
+        getView().getDisplayDriver().display(taxonomy);
+        getView().setState(State.DISPLAYING, getPermission());
+        if (stats == null) {
+            taxonomyManager.findStats(new Receiver<TaxonomyStatsProxy>() {
 
-			@Override
-			public void onFailure(ServerFailure error) {
-				fireEvent(new LoadingIndicatorEvent(false));
-				getProxy().manualRevealFailed();
-				placeManager.revealPlace(new PlaceRequest(NameTokens.taxonomies));
-			}
-		};
-		try {
-			Long taxonomyId = Long.valueOf(placeRequest.getParameter("id",
-					null));
-			if (taxonomy == null || !taxonomy.getId().equals(taxonomyId)) {
-				stats = null;
-				taxonomyManager.findOne(receiver, taxonomyId);
-			} else {
-				getProxy().manualReveal(TaxonomyDetailPresenter.this);
-			}
-		} catch (NumberFormatException e) {
-			getProxy().manualRevealFailed();
-			placeManager.revealPlace(new PlaceRequest(NameTokens.taxonomies));
-		}
-	}
-	
-	@Override
-	public boolean useManualReveal() {
-		return true;
-	}
-	
+                @Override
+                public void onSuccess(TaxonomyStatsProxy response) {
+                    stats = response;
+                    displayStats();
+                }
 
-	@Override
-	public void onEdit() {
-		getView().setState(State.EDITING,getPermission());
-		TaxonomyRequest ctx = taxonomyManager.getContext();
-		getView().getEditDriver().edit(taxonomy, ctx);
-		ctx.save(taxonomy).to(receiver);
-	}
+            }, taxonomy.getId());
+        } else {
+            displayStats();
+        }
+    }
 
-	@Override
-	public void onSave() {
-		getView().setState(State.SAVING,getPermission());
-		RequestContext req = getView().getEditDriver().flush();
-		req.fire();
-	}
+    @Override
+    public void prepareFromRequest(PlaceRequest placeRequest) {
+        super.prepareFromRequest(placeRequest);
+        LoadingIndicatorEvent.fire(this, true);
+        Receiver<TaxonomyProxy> receiver = new Receiver<TaxonomyProxy>() {
+            @Override
+            public void onSuccess(TaxonomyProxy tax) {
+                taxonomy = tax;
+                fireLoadEvent = true;
+                getProxy().manualReveal(TaxonomyDetailPresenter.this);
+            }
 
-	@Override
-	public void onCancel() {
-		getView().setState(State.DISPLAYING,getPermission());
-		getView().getDisplayDriver().display(taxonomy);
-	}
-	
-	private int getPermission() {
-		int permission = 0;
-		if (currentUser.isAdmin()) {
-			permission = AccessControlEntryProxy.EDIT;
-		}
-		return permission;
-	}
-	
-	private void displayStats() {
-		if (stats == null)
-			return;
-		getView().setGeoChartData(DataTableUtils.createDataTableFromString(stats.getGeoChartData()));
-		getView().setAlleleAssayData(DataTableUtils.createDataTableFromString(stats.getAlleleAssayData()));
-		getView().setSampStatData(DataTableUtils.createDataTableFromString(stats.getSampStatData()));
-		getView().setStockGenerationData(DataTableUtils.createDataTableFromString(stats.getStockGenerationData()));
-		getView().scheduleLayout();
-	}
+            @Override
+            public void onFailure(ServerFailure error) {
+                fireEvent(new LoadingIndicatorEvent(false));
+                getProxy().manualRevealFailed();
+                placeManager.revealPlace(new PlaceRequest(NameTokens.taxonomies));
+            }
+        };
+        try {
+            Long taxonomyId = Long.valueOf(placeRequest.getParameter("id",
+                    null));
+            if (taxonomy == null || !taxonomy.getId().equals(taxonomyId)) {
+                stats = null;
+                taxonomyManager.findOne(receiver, taxonomyId);
+            } else {
+                getProxy().manualReveal(TaxonomyDetailPresenter.this);
+            }
+        } catch (NumberFormatException e) {
+            getProxy().manualRevealFailed();
+            placeManager.revealPlace(new PlaceRequest(NameTokens.taxonomies));
+        }
+    }
+
+    @Override
+    public boolean useManualReveal() {
+        return true;
+    }
+
+
+    @Override
+    public void onEdit() {
+        getView().setState(State.EDITING, getPermission());
+        TaxonomyRequest ctx = taxonomyManager.getContext();
+        getView().getEditDriver().edit(taxonomy, ctx);
+        ctx.save(taxonomy).to(receiver);
+    }
+
+    @Override
+    public void onSave() {
+        getView().setState(State.SAVING, getPermission());
+        RequestContext req = getView().getEditDriver().flush();
+        req.fire();
+    }
+
+    @Override
+    public void onCancel() {
+        getView().setState(State.DISPLAYING, getPermission());
+        getView().getDisplayDriver().display(taxonomy);
+    }
+
+    private int getPermission() {
+        int permission = 0;
+        if (currentUser.isAdmin()) {
+            permission = AccessControlEntryProxy.EDIT;
+        }
+        return permission;
+    }
+
+    private void displayStats() {
+        if (stats == null)
+            return;
+        getView().setGeoChartData(DataTableUtils.createChartDataTableFromString(stats.getGeoChartData()));
+        getView().setAlleleAssayData(DataTableUtils.createChartDataTableFromString(stats.getAlleleAssayData()));
+        getView().setSampStatData(DataTableUtils.createChartDataTableFromString(stats.getSampStatData()));
+        getView().setStockGenerationData(DataTableUtils.createChartDataTableFromString(stats.getStockGenerationData()));
+        getView().scheduleLayout();
+    }
 }
