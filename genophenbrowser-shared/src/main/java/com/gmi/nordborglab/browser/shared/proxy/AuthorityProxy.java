ff72610a79bd557dc22520e11b4e6c5d23be5766
Ümit Seren <uemit.seren@gmail.com>
2013-11-06 16:56:30 +0100

    Bugfix: The userId of the embedded composite key is properly set
     Make sure that the userId field in the embedded key is properly set when an instance of
     AppUserProxy/AppUser is sent from the client to the server.

--- genophenbrowser-shared/src/main/java/com/gmi/nordborglab/browser/shared/proxy/AuthorityProxy.java
+++ genophenbrowser-shared/src/main/java/com/gmi/nordborglab/browser/shared/proxy/AuthorityProxy.java
@@ -4,8 +4,13 @@ package com.gmi.nordborglab.browser.shared.proxy;
 import com.google.web.bindery.requestfactory.shared.ProxyForName;
 import com.google.web.bindery.requestfactory.shared.ValueProxy;
 
-@ProxyForName(value="com.gmi.nordborglab.browser.server.domain.acl.Authority")
-public interface AuthorityProxy extends ValueProxy{
-	String getAuthority();
-	void setAuthority(String authority);
+@ProxyForName(value = "com.gmi.nordborglab.browser.server.domain.acl.Authority")
+public interface AuthorityProxy extends ValueProxy {
+    String getAuthority();
+
+    void setAuthority(String authority);
+
+    void setUserId(Long id);
+
+    Long getUserId();
 }
