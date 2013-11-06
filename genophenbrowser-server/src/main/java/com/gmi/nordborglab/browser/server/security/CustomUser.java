156862a02ef7f8107533c6f744ad979bc02ceba9
Ümit Seren <uemit.seren@gmail.com>
2013-11-06 16:53:04 +0100

    Enhancement: Add AppUser to the CurrentUser
    Instead of storing single fields in CurrentUser, store the entire AppUser instance. This way
    the Authentication instance has access to the entire AppUser instance.

--- genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/security/CustomUser.java
+++ genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/security/CustomUser.java
@@ -2,47 +2,54 @@ package com.gmi.nordborglab.browser.server.security;
 
 import java.util.Collection;
 
+import com.gmi.nordborglab.browser.server.domain.acl.AppUser;
+import com.google.common.base.Preconditions;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.SpringSecurityCoreVersion;
 import org.springframework.security.core.userdetails.User;
 
 public class CustomUser extends User {
-	
-	private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
-	
-	private String email;
-    private String firstname;
-    private String lastname;
 
-	public CustomUser(String firstname,String lastname,String email,String username, String password, boolean enabled,
-			boolean accountNonExpired, boolean credentialsNonExpired,
-			boolean accountNonLocked,
-			Collection<? extends GrantedAuthority> authorities) {
-		super(username, password, enabled, accountNonExpired, credentialsNonExpired,
-				accountNonLocked, authorities);
-		
-		this.email = email;
-		this.firstname = firstname;
-		this.lastname = lastname;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public String getFirstname() {
-		return firstname;
-	}
-	
-	public String getLastname() {
-		return lastname;
-	}
-	
-	public String getJson() {
-		return SecurityUtil.serializeUserToJson(this);
-	}
+    private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+
+    private AppUser appUser;
+
+    public CustomUser(AppUser appUser,
+                      Collection<? extends GrantedAuthority> authorities) {
+        super(appUser.getId().toString(), appUser.getPassword(), appUser.isEnabled(), appUser.isAccountNonExpired(), appUser.isCredentialsNonExpired(),
+                appUser.isAccountNonLocked(), authorities);
+        this.appUser = appUser;
+    }
+
+    public Long getId() {
+        return appUser.getId();
+    }
+
+    public String getEmail() {
+        return appUser.getEmail();
+    }
+
+    public String getFirstname() {
+        return appUser.getFirstname();
+    }
+
+    public String getLastname() {
+        return appUser.getLastname();
+    }
+
+    public String getJson() {
+        return SecurityUtil.serializeUserToJson(this);
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser user) {
+        this.appUser = user;
+    }
 
     public String getFullName() {
-        return (String.format("%s %s",firstname,lastname)).trim();
+        return (String.format("%s %s", getFirstname(), getLastname())).trim();
     }
 }
