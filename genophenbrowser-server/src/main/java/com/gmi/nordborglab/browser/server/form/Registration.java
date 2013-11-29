0a889c0e72063265f0ad2926522288752555efb1
Ümit Seren <uemit.seren@gmail.com>
2013-11-29 12:54:55 +0100

    Bugfix: Custom message for password validator

--- genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/form/Registration.java
+++ genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/form/Registration.java
@@ -4,18 +4,18 @@ import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 
 public class Registration {
 
-    @NotEmpty
-    @Size(min = 4, max = 20)
+    @Length(min = 4, max = 20, message = "Password must be between 4 and 20 characters long.")
     private String password;
-    
+
     @NotEmpty
     private String confirmPassword;
-    
+
     @NotEmpty
     @Email
     private String email;
@@ -23,54 +23,54 @@ public class Registration {
     private String firstname;
     @NotEmpty
     private String lastname;
-    
 
-    @AssertTrue(message="Password does not match")
-    public boolean isPasswordValid(){
-            if (password == null) {
-                return false;
-            } else {
-                return password.equals(confirmPassword);
-           }
+
+    @AssertTrue(message = "Password does not match")
+    public boolean isPasswordValid() {
+        if (password == null) {
+            return false;
+        } else {
+            return password.equals(confirmPassword);
         }
-    
+    }
+
     public void setFirstname(String firstname) {
-    	this.firstname = firstname;
+        this.firstname = firstname;
     }
-    
+
     public String getFirstname() {
-    	return firstname;
+        return firstname;
     }
-    
+
     public void setLastname(String lastname) {
-    	this.lastname = lastname;
+        this.lastname = lastname;
     }
-    
+
     public String getLastname() {
-    	return lastname;
+        return lastname;
     }
 
     public void setPassword(String password) {
-            this.password = password;
+        this.password = password;
     }
 
     public String getPassword() {
-            return password;
+        return password;
     }
 
     public void setConfirmPassword(String confirmPassword) {
-            this.confirmPassword = confirmPassword;
+        this.confirmPassword = confirmPassword;
     }
 
     public String getConfirmPassword() {
-            return confirmPassword;
+        return confirmPassword;
     }
 
     public void setEmail(String email) {
-            this.email = email;
+        this.email = email;
     }
 
     public String getEmail() {
-            return email;
+        return email;
     }
 }
