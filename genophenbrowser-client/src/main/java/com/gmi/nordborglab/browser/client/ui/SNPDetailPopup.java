32caa92187a8f2d97ca20a83ac8c0c6b7a33fb30
Ümit Seren <uemit.seren@gmail.com>
2014-10-22 14:58:37 +0200

    Enhancement #56: SNPDetailPopup added
    Uses PopupPanel with some custom UI (similar to bootstraps popover)

---
+++ genophenbrowser-client/src/main/java/com/gmi/nordborglab/browser/client/ui/SNPDetailPopup.java
@@ -0,0 +1,61 @@
+package com.gmi.nordborglab.browser.client.ui;
+
+import com.gmi.nordborglab.browser.client.place.NameTokens;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.HeadingElement;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.InlineHyperlink;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import com.gwtplatform.mvp.client.proxy.PlaceManager;
+import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
+
+/**
+ * Created by uemit.seren on 10/21/14.
+ */
+public class SNPDetailPopup extends PopupPanel {
+
+    interface Binder extends UiBinder<Widget, SNPDetailPopup> {
+    }
+
+    interface MyStyle extends CssResource {
+        String popup();
+    }
+
+    private static Binder ourUiBinder = GWT.create(Binder.class);
+    protected Integer chromosome;
+    protected Integer position;
+    protected final PlaceManager placeManager;
+    protected final PlaceRequest snpDetailRequest = new PlaceRequest.Builder().nameToken(NameTokens.snps).build();
+    @UiField
+    InlineHyperlink snpInfoLink;
+    @UiField
+    HeadingElement snpInfo;
+
+    @UiField
+    MyStyle style;
+
+    @Inject
+    public SNPDetailPopup(PlaceManager placeManager) {
+        super();
+        this.placeManager = placeManager;
+        setWidget(ourUiBinder.createAndBindUi(this));
+        setStylePrimaryName(style.popup());
+        setAnimationEnabled(false);
+        setAutoHideEnabled(true);
+        setAutoHideOnHistoryEventsEnabled(true);
+
+    }
+
+    public void setDataPoint(Long analysisId, Integer chromosome, Integer position) {
+        this.chromosome = chromosome;
+        this.position = position;
+        final PlaceRequest request = new PlaceRequest.Builder(snpDetailRequest).with("analysisid", String.valueOf(analysisId)).with("snpid", String.valueOf(chromosome) + "_" + String.valueOf(position)).build();
+        snpInfo.setInnerText(String.valueOf(chromosome) + " : " + String.valueOf(position));
+        snpInfoLink.setTargetHistoryToken(placeManager.buildHistoryToken(request));
+    }
+
+}
