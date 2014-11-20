1bf7e6e397eb5a6f61a6ccd2acf1c62acb31b7a4
Ümit Seren <uemit.seren@gmail.com>
2014-11-20 13:05:00 +0100

    Enhancement #56: Support for GenotypeReader added
     A new interface GenotypeReader and an implementation for HDF5
     (HDF5GenotypeReader) was added. This allows to retrieve alleles for a specific
      allele_assay either for all accessions or for a list of accessions.

---
+++ genophenbrowser-server/src/main/java/com/gmi/nordborglab/browser/server/data/hdf5/HDF5GenotypeReader.java
@@ -0,0 +1,85 @@
+package com.gmi.nordborglab.browser.server.data.hdf5;
+
+import ch.systemsx.cisd.hdf5.HDF5DataSetInformation;
+import ch.systemsx.cisd.hdf5.HDF5Factory;
+import ch.systemsx.cisd.hdf5.IHDF5Reader;
+import com.gmi.nordborglab.browser.server.data.GenotypeReader;
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Ints;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+
+/**
+ * Created by uemit.seren on 10/22/14.
+ */
+
+@Component
+public class HDF5GenotypeReader implements GenotypeReader {
+
+    private
+    @Value("${GENOTYPE.data_folder}")
+    String GENOTYPE_DATA_FOLDER;
+
+    public HDF5GenotypeReader() {
+    }
+
+
+    @Override
+    public List<Byte> getAlleles(String genotype, Integer chr, Integer position, LinkedHashSet<String> passportIds) {
+        IHDF5Reader reader = getReader(genotype);
+        List<Byte> data = getAlleles(reader, chr, position, passportIds);
+        reader.close();
+        return data;
+    }
+
+    private List<Byte> getAlleles(IHDF5Reader reader, Integer chr, Integer position, LinkedHashSet<String> passportIds) {
+        HDF5DataSetInformation info = reader.object().getDataSetInformation("snps");
+        final long accessionCount = info.getNumberOfElements();
+        List<Byte> alleles;
+        List<String> accessionIds = Lists.newArrayList(reader.string().readArray("accessions"));
+        List<Integer> indices = Lists.newArrayList();
+        final int chr_ix = chr - 1;
+        // get the indices of the passportIds
+        if (passportIds != null) {
+            for (String passportId : passportIds) {
+                int ix = accessionIds.indexOf(passportId);
+                if (ix >= 0) {
+                    indices.add(ix);
+                }
+            }
+        }
+
+        // get the position index;
+        final int[] chr_region = reader.int32().getMatrixAttr("positions", "chr_regions")[chr_ix];
+        final int block_size = chr_region[1] - chr_region[0];
+        final List<Integer> positions = Ints.asList(reader.int32().readArrayBlockWithOffset("positions", block_size, chr_region[0]));
+        final int pos_ix = positions.indexOf(position);
+        if (pos_ix == -1)
+            throw new RuntimeException(String.format("no SNP at position %s found", position));
+        // get entire snps
+
+        final byte[] snps = reader.int8().readMatrixBlockWithOffset("snps", 1, (int) accessionCount, pos_ix, 0)[0];
+
+        if (passportIds != null) {
+            alleles = Lists.newArrayList();
+            for (Integer ix : indices) {
+                alleles.add(snps[ix]);
+            }
+        } else {
+            alleles = Bytes.asList(snps);
+        }
+        return alleles;
+
+    }
+
+    protected IHDF5Reader getReader(String file) {
+        IHDF5Reader reader = HDF5Factory.openForReading(GENOTYPE_DATA_FOLDER + File.separator + file + "/all_chromosomes_binary.hdf5");
+        return reader;
+    }
+}
